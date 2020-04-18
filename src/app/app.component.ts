001b63d9d5c9efa39965cbf64fb380a3bfb2b7d0
rmpweb <rmuscatphotography@gmail.com>
2020-04-18 20:22:56 +0200

    updates routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import data from '../data.json';
-import {Observable} from "rxjs";
-import {DataService} from "./services/data.service";
-import {Router} from "@angular/router";
+import {Location} from '@angular/common';
+import {Observable} from 'rxjs';
+import {DataService} from './services/data.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'rm-root',
@@ -12,16 +12,36 @@ import {Router} from "@angular/router";
 export class AppComponent {
 
   isNavMenuOpen = false;
+  public isNavigating = false;
+  public fragment: string;
   public currentSection = 'section1';
   public data$: Observable<any>;
-  constructor(private dataService: DataService, private router: Router){
+  constructor(private dataService: DataService, private location: Location) {
     this.data$ = dataService.getContent();
   }
 
   onSectionChange(sectionId: string) {
-    this.currentSection = sectionId;
-    this.router.navigate([], {fragment: sectionId});
+    if (this.isNavigating) {
+      if (sectionId === this.fragment) {
+        this.updateLocation(sectionId);
+      }
+    } else {
+      this.updateLocation(sectionId);
+    }
+
     console.log(sectionId);
+  }
+
+  updateLocation(sectionId: string) {
+    this.currentSection = sectionId;
+    if (sectionId === 'home') {
+      this.location.replaceState('');
+      // this.router.navigate([]);
+    } else {
+      this.location.replaceState('#' + sectionId);
+      // this.router.navigate([], {fragment: sectionId});
+    }
+    this.isNavigating = false;
   }
 
   onMenuClick() {
@@ -29,7 +49,9 @@ export class AppComponent {
   }
 
   scrollToElement(element) {
+    this.isNavigating = true;
+    this.fragment = element.id;
     this.isNavMenuOpen = false;
-    // element.scrollIntoView({behavior: 'smooth', block: 'start'});
+    element.scrollIntoView({behavior: 'smooth', block: 'start'});
   }
 }
